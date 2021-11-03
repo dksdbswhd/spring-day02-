ef3e9cbec1713e60d9e70190a21598e044b5b083
Anyunjong <dksdbswhd12@naver.com>
2021-11-03 11:41:58 +0900

    day02

--- src/main/java/jcpdev/day02/mapper/FreeboardMapper.java
+++ src/main/java/jcpdev/day02/mapper/FreeboardMapper.java
@@ -7,7 +7,7 @@ import jcpdev.day02.dto.BoardDto;
 import jcpdev.day02.dto.PageDto;
 
 
-public interface FreeboardMapper {   
+public interface FreeboardMapper {
 	//mapper 로 dao 동작을 하도록 설정할 것입니다.-> **spring mybatis 에서 구현체를 생성하여 bean 등록합니다.**
 	//sql 실행에 필요한 메소드 시그니처(프로토타입,원형) 정의. 메소드 이름은 mapper xml 의 id와 동일하게 합니다.
 	int getCount();			//테이블 데이터 레코드 전체 갯수
